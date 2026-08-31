f3ad735a554a2717a1a23ddfaf3600877922dd2e
Bzona10 <bzona@pioix.edu.ar>
2026-08-31 11:43:19 -0300

    ejercicios 2 y 3

--- trabajo/src/app/socket/page.js
+++ trabajo/src/app/socket/page.js
@@ -1,19 +1,92 @@
 "use client"
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useSocket } from "@/hooks/useSocket";
+
 export default function SocketPage() {
-    const [isConnected, setIsConnected] = useState(false);}
+    const { socket, isConnected } = useSocket();
+    const [mensajes, setMensajes] = useState([]);
+    const [contador, setContador] = useState(0);
+
+
+    useEffect(() => {
+
+        if (socket) {
+
+            console.log("Socket disponible:", socket);
+
+
+            socket.on("pingAll", (data) => {
+
+                setMensajes((mensajesAnteriores) => [
+                    ...mensajesAnteriores,
+                    data
+                ]);
+            });
 
 
 
-    return(
+            socket.on("respuestaPersonalizada", (data) => {
+                setContador(data.contador);
+                console.log("Contador:", data.contador);
+            });
+            return () => {
+                socket.off("pingAll");
+                socket.off("respuestaPersonalizada");
+            };
+        }
+
+    }, [socket]);
+
+
+    function sendMessage() {
+
+        if (socket) {
+
+            socket.emit("pingAll", {
+                msg: "Hola desde mi compu"
+            });
+        }
+    }
+
+
+    function contar() {
+        if (socket) {
+            socket.emit("eventoPersonalizado");
+        }
+    }
+
+
+    return (
         <>
-        { isConnected ?(
-            <p>🟢 Conectado al servidor</p>
-        ) :(
-            <p>🔴 Desconectado</p>
-        )}
-        
-        
+
+            {isConnected ? (
+                <p>🟢 Conectado al servidor</p>
+            ) : (
+                <p>🔴 Desconectado</p>
+            )}
+
+
+            <button onClick={sendMessage}>
+                Enviar ping a todos
+            </button>
+
+            <h2>Mensajes recibidos:</h2>
+            <ul>
+                {mensajes.map((mensaje, index) => (
+                    <li key={index}>
+                        {mensaje.message.msg}
+                    </li>
+                ))}
+            </ul>
+
+
+
+            <button onClick={contar}>
+                Contar +1
+            </button>
+            <h2>Contador: {contador}</h2>
+
         </>
-    )
+    );
+}

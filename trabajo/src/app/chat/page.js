708594c290cb376d7f1d03706f1695afe5fc53d4
Bzona10 <bzona@pioix.edu.ar>
2026-09-02 10:11:25 -0300

    ejercicio 6

---
+++ trabajo/src/app/chat/page.js
@@ -0,0 +1,122 @@
+"use client"
+
+import { useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation";
+import { useSocket } from "@/hooks/useSocket";
+
+export default function SocketPage() {
+
+    // Leer los datos de la URL
+    const searchParams = useSearchParams();
+
+    const { socket, isConnected } = useSocket();
+
+    const sala = searchParams.get("sala");
+    const usuario = searchParams.get("usuario");
+
+    // Estados
+    const [mensaje, setMensaje] = useState("");
+    const [conversacion, setConversacion] = useState([]);
+
+
+    // Unirse a la sala
+    useEffect(() => {
+
+        if (socket && sala) {
+
+            socket.emit("joinRoom", {
+                room: sala
+            });
+
+        }
+
+    }, [socket, sala]);
+
+
+    // Escuchar mensajes nuevos
+    useEffect(() => {
+
+        if (socket) {
+
+            socket.on("newMessage", (data) => {
+
+                setConversacion((mensajesAnteriores) => [
+                    ...mensajesAnteriores,
+                    data
+                ]);
+
+            });
+
+            return () => {
+                socket.off("newMessage");
+            };
+        }
+
+    }, [socket]);
+
+
+    // Enviar mensaje
+    function enviarMensaje() {
+
+        if (socket && mensaje.trim() !== "") {
+
+            socket.emit("sendMessage", {
+                message: mensaje
+            });
+
+            setMensaje("");
+        }
+    }
+
+
+    return (
+        <>
+
+            {isConnected ? (
+                <p>🟢 Conectado al servidor</p>
+            ) : (
+                <p>🔴 Desconectado</p>
+            )}
+
+
+            <h1>Chat</h1>
+
+            <p>
+                Usuario: <strong>{usuario}</strong>
+            </p>
+
+            <p>
+                Sala: <strong>{sala}</strong>
+            </p>
+
+
+            <input
+                type="text"
+                value={mensaje}
+                onChange={(e) => setMensaje(e.target.value)}
+                placeholder="Escribí un mensaje"
+            />
+
+            <button onClick={enviarMensaje}>
+                Enviar
+            </button>
+
+
+            <h2>Conversación:</h2>
+
+            <ul>
+
+                {conversacion.map((data, index) => (
+
+                    <li key={index}>
+                        Sala: {data.room} - {data.message}
+                    </li>
+
+                ))}
+
+            </ul>
+
+        </>
+    );
+}
+
